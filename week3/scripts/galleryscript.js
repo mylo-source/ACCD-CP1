94a00ce46d4f8b09c3a3b6acbbd614e85c45195e
mylo-source <manekiamyra@gmail.com>
2025-10-04 18:51:18 -0700

    Week 3 Explorations

---
+++ week3/scripts/galleryscript.js
@@ -0,0 +1,52 @@
+//let gT1 = document.getElementById("galleryThumb1")
+//let gT2 = document.getElementById("galleryThumb2")
+//let gT3 = document.getElementById("galleryThumb3")
+//let gT4 = document.getElementById("galleryThumb4")
+let thumbImages = document.querySelectorAll(".gallery-thumb > img")
+console.log(thumbImages)
+let scImage = document.getElementById("showcaseImage")
+let arrRight = document.querySelector(".showcase-arrow-right")
+let arrLeft = document.querySelector(".showcase-arrow-left")
+
+let currentIndexThumb = 0
+
+function changeImage(_image) {
+    let thumbSrc = _image.src
+    scImage.src = thumbSrc
+
+    thumbImages.forEach(function(elem){
+        elem.parentElement.classList.remove("current-thumb")
+    })
+
+    _image.parentElement.classList.add("current-thumb")
+
+    currentIndexThumb = Array.from(thumbImages).indexOf(_image)
+}
+
+for(let i = 0; i < thumbImages.length; i ++){
+    thumbImages[i].addEventListener("click", function(event){
+        changeImage(event.target)
+
+    })
+}
+
+arrRight.addEventListener("click", function(){
+    currentIndexThumb++
+    if (currentIndexThumb >= thumbImages.length){
+        currentIndexThumb = 0
+    }
+    changeImage(thumbImages[currentIndexThumb])
+})
+
+arrLeft.addEventListener("click", function(){
+    currentIndexThumb--
+    if (currentIndexThumb < 0){
+        currentIndexThumb = thumbImages.length - 1
+    }
+    changeImage(thumbImages[currentIndexThumb])
+})
+
+//gT1.addEventListener("click", changeImage)
+//gT2.addEventListener("click", changeImage)
+//gT3.addEventListener("click", changeImage)
+//gT4.addEventListener("click", changeImage)
---
+++ week3/scripts/script.js
@@ -0,0 +1,36 @@
+let cStage = document.getElementById ("colorStage");
+let cButton = document.getElementById("colorButton");
+
+
+const qImage = document.getElementById("quokkaImage")
+const qButton = document.getElementById("imageToggle")
+const tButton = document.getElementById("triggerImage")
+
+let changeColor = function()
+{
+
+    let rComp = Math.random() * 255
+    let gComp = Math.random() * 255
+    let bComp = Math.random() * 255
+
+    cStage.style.backgroundColor = "rgb(" + rComp + ", " + gComp + ", " + bComp + ")"
+
+}
+
+let toggleImage = () =>
+{
+    console.log(qImage.src)
+    if(qImage.src.includes("quokka1")){
+        qImage.src = "images/quokka2.jpg"
+    }
+    else{
+        qImage.src = "images/quokka1.jpg"
+    }
+}
+
+setInterval(changeColor, 500)
+
+qButton.addEventListener("click", toggleImage)
+tButton.addEventListener("click", toggleImage)
+cButton.addEventListener("click", changeColor)
+window.addEventListener("load", changeColor)
